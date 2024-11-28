0db7bd525a59f91467a236a1c0271514d195cbf9
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-11-28 17:27:10 +0100

    refactor from `.atoms` to use `.frames`

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import ipsuite as ips
 project = ips.Project()
 
 with project:
-    water = ips.nodes.AddDataH5MD(file="data/water.h5", name="water")
+    water = ips.AddDataH5MD(file="data/water.h5", name="water")
     bmim_bf4 = ips.AddData(file="data/BMIM_BF4_363_15K.extxyz", name="BMIM_BF4_363_15K")
 
 project.repro()
