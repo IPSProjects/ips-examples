71521f165251141460342e15696a6d76bf02ed22
PythonFZ <fabian.zills@web.de>
2024-12-03 13:54:37 +0100

    gromacs example

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ips-examples
+
+Requires
+
+- `conda install -c conda-forge acpype`
+- gromacs installation
+- `pip install pyedr`
--- main.py
+++ main.py
@@ -6,4 +6,16 @@ with project:
     water = ips.AddDataH5MD(file="data/water.h5", name="water")
     bmim_bf4 = ips.AddData(file="data/BMIM_BF4_363_15K.extxyz", name="BMIM_BF4_363_15K")
 
-project.repro()
+with project.group("gromacs"):
+    system = ips.Smiles2Gromacs(
+        smiles=["CCCCN1C=C[N+](=C1)C", "CC(=O)[O-]"],
+        density=0.9,
+        count=[16, 16],
+        labels=["Imx", "BFx"],
+        config_files=["config/gromacs/gmx_em.yaml"],
+        mdp_files=["config/gromacs/gmx_md.mdp"],
+        fudgeLJ=1.0,
+        fudgeQQ=1.0,
+    )
+
+project.build()
